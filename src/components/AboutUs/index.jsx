2e33d9d540c87f196ac5f546813beccfde39d9ac
Qstimo <vadtolpekin@gmail.com>
2024-04-17 07:24:37 +0300

    feat: aboutUs block

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import cls from './aboutUs.module.scss'
+
+const aboutUsList = [
+    { title: 'более 40', text: "видов рыб" },
+    { title: '10 000', text: "довольных клиентов" },
+    { title: '15', text: "лет успешной работы" },
+    { title: '500', text: "более 500 веселых корпоративов" }
+]
 
 const AboutUs = () => {
     return (
-        <div>AboutUs</div>
+        <div>
+            <div className="content">
+                <div className={cls.AboutUs_content}>
+                    {aboutUsList.map(list => (
+                        <div className={cls.AboutUs_item}>
+                            <h3>{list.title}</h3>
+                            <p>{list.text}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
